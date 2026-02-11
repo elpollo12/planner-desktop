a99da78a2b27a59d86585787f7d009355e011432
José Hernández <josehernandez12@gmail.com>
2026-02-11 08:25:59 -0400

    Ajuste de Sync

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -80,6 +80,7 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Ingrese su contraseña"
             required
+            allowSubmitOnEnter
           />
 
           {error && (
